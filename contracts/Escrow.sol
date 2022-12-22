fc70cbfc6e85398b2cd2312f4416fdc18f9f7a84
turnMeUpSon <milleryan2003@gmail.com>
2022-12-22 15:45:10 +0300

    Writed some deployment tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -39,4 +39,24 @@ contract Escrow {
         inspector = _inspector;
         lender = _lender;
     }
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // Transfer NFT from seller to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    // Put Under Contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
 }
