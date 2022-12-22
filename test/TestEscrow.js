9e1ab080ba1fd287750f14669a73565680736ec2
turnMeUpSon <milleryan2003@gmail.com>
2022-12-22 15:04:25 +0300

    Created Real Estate Contract with NFT implementation

--- test/TestEscrow.js
+++ test/TestEscrow.js
@@ -1,10 +1,13 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
-
-})
+describe("Escrow", () => {
+  describe("saves the addresses", async function () {
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+  });
+});
